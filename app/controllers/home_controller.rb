ebff737f89603e536b12d3eaf0585d85ed8004c5
joojieman <jamespatrickpe@gmail.com>
2015-10-22 10:32:25 +0800

    gdfasdfad

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,4 +22,21 @@ class HomeController < ApplicationController
   def index
   end
 
+  def parts_library
+  end
+
+
+  def become_a_supplier
+  end
+
+  def become_a_retailer
+  end
+
+  def store_locations
+  end
+
+  def about_us
+  end
+
+
 end
