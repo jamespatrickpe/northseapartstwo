4092111050ae168d3ba8867186e5a12dd1b5192b
James Patrick G. Pe <jamespatrickpe@gmail.com>
2015-10-23 13:32:35 -0700

    dfasdf

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-// MANUALLY PUT EVERY JAVASCRIPT
+// Manually put every javascript
 //= require jquery
 //= // require jquery_ujs
 //= require jquery-ui
