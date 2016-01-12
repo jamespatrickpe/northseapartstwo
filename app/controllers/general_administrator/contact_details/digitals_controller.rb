20fcca33b008689cdfed88accdb5731f2066bdfc
Jc Gofredo <john.gofredo@novare.com.hk>
2016-01-12 12:58:51 +0800

    Address CRUD - no mapping with ACTOR

---
+++ app/controllers/general_administrator/contact_details/digitals_controller.rb
@@ -0,0 +1,31 @@
+class GeneralAdministrator::ContactDetails::DigitalsController < GeneralAdministrator::ContactDetailsController
+
+  def index
+
+  end
+
+  def delete
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def process_digital_form(myAddress)
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+end
---
+++ app/controllers/general_administrator/contact_details/telephones_controller.rb
@@ -0,0 +1,31 @@
+class GeneralAdministrator::ContactDetails::TelephonesController < GeneralAdministrator::ContactDetailsController
+
+  def index
+
+  end
+
+  def delete
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def process_telephone_form(myTelephone)
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+end
