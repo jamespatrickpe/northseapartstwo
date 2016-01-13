c4cdf7ab5e49de00b4268bb4dc83f0240c66ef30
James Patrick G. Pe <jamespatrickpe@gmail.com>
2016-01-13 09:33:51 +0800

    dfasdfsadf

--- app/controllers/human_resources/compensation_and_benefits/institutional_adjustments_controller.rb
+++ app/controllers/human_resources/compensation_and_benefits/institutional_adjustments_controller.rb
@@ -3,16 +3,17 @@ class HumanResources::CompensationAndBenefits::InstitutionalAdjustmentsControlle
   def index
     query = generic_table_aggregated_queries('institutional_adjustments','institutional_adjustments.created_at')
     begin
-      @base_rates = BaseRate
-        .where("actors.name LIKE ? OR " +
-                 "institutional_adjustments.id LIKE ? OR " +
-                 "institutional_adjustments.name LIKE ? OR " +
-                 "institutional_adjustments.description LIKE ? OR " +
-                 "institutional_adjustments.logo LIKE ? OR " +
+      @institutional_adjustments = InstitutionalAdjustment
+        .where("institutional_adjustments.id LIKE ? OR " +
+                 "institutional_adjustments.institution LIKE ? OR " +
+                 "institutional_adjustments.contribution_type LIKE ? OR " +
+                 "institutional_adjustments.start_range LIKE ? OR " +
+                 "institutional_adjustments.end_range LIKE ? OR " +
+                 "institutional_adjustments.employer_contribution LIKE ? OR " +
+                 "institutional_adjustments.employee_contribution LIKE ? OR " +
                  "institutional_adjustments.period_of_time LIKE ? OR " +
-                 "institutional_adjustments.remark LIKE ? OR " +
-                 "institutional_adjustments.start_of_effectivity LIKE ? OR " +
-                 "institutional_adjustments.end_of_effectivity LIKE ? OR " +
+                 "institutional_adjustments.description LIKE ? OR " +
+                 "institutional_adjustments.date_of_effectivity LIKE ? OR " +
                  "institutional_adjustments.created_at LIKE ? OR " +
                  "institutional_adjustments.updated_at LIKE ?",
              "%#{query[:search_field]}%",
@@ -25,75 +26,70 @@ class HumanResources::CompensationAndBenefits::InstitutionalAdjustmentsControlle
              "%#{query[:search_field]}%",
              "%#{query[:search_field]}%",
              "%#{query[:search_field]}%",
+             "%#{query[:search_field]}%",
              "%#{query[:search_field]}%")
       .order(query[:order_parameter] + ' ' + query[:order_orientation])
-      @base_rates = Kaminari.paginate_array(@base_rates).page(params[:page]).per(query[:current_limit])
-    rescue
-      flash[:general_flash_notification] = "Error has Occured"
+      @institutional_adjustments = Kaminari.paginate_array(@institutional_adjustments).page(params[:page]).per(query[:current_limit])
+    rescue => ex
+      flash[:general_flash_notification] = "Error has Occured" + ex.to_s
     end
-    render 'human_resources/compensation_and_benefits/base_rates/index'
+    render 'human_resources/compensation_and_benefits/institutional_adjustments/index'
   end
 
   def search_suggestions
-    baseRates = BaseRate.includes(employee: :actor).where("actors.name LIKE (?)", "%#{ params[:query] }%").pluck("actors.name")
-    direct = "{\"query\": \"Unit\",\"suggestions\":" + baseRates.uniq.to_s + "}" # default format for plugin
+    institutional_adjustments = InstitutionalAdjustment.pluck("institutional_adjustments.contribution_type")
+    direct = "{\"query\": \"Unit\",\"suggestions\":" + institutional_adjustments.uniq.to_s + "}" # default format for plugin
     respond_to do |format|
       format.all { render :text => direct}
     end
   end
 
   def new
-    initialize_employee_selection
-    @selected_base_rate = BaseRate.new
-    render 'human_resources/compensation_and_benefits/base_rates/base_rate_form'
+    @selected_institutional_adjustment = InstitutionalAdjustment.new
+    render 'human_resources/compensation_and_benefits/institutional_adjustments/institutional_adjustment_form'
   end
 
   def edit
-    initialize_employee_selection
-    @selected_base_rate = BaseRate.find(params[:id])
-    render 'human_resources/compensation_and_benefits/base_rates/base_rate_form'
+    @selected_institutional_adjustment = InstitutionalAdjustment.find(params[:id])
+    render 'human_resources/compensation_and_benefits/institutional_adjustments/institutional_adjustment_form'
   end
 
-  def process_base_rate_form(baseRate)
+  def process_institutional_adjustment_form(institutional_adjustment)
     begin
-      baseRate.employee_id = params[:base_rate][:employee_id]
-      employee = Employee.find(params[:base_rate][:employee_id])
-      baseRate.employee = employee
-      baseRate.signed_type = params[:base_rate][:signed_type]
-      baseRate.amount = params[:base_rate][:amount]
-      baseRate.period_of_time = params[:base_rate][:period_of_time]
-      baseRate.rate_type = params[:base_rate][:rate_type]
-      baseRate.remark = params[:base_rate][:remark]
-      baseRate.start_of_effectivity = params[:base_rate][:start_of_effectivity]
-      baseRate.end_of_effectivity = params[:base_rate][:end_of_effectivity]
-      baseRate.save!
+      institutional_adjustment.institution = params[:institutional_adjustment][:institution]
+      institutional_adjustment.contribution_type = params[:institutional_adjustment][:contribution_type]
+      institutional_adjustment.start_range = params[:institutional_adjustment][:start_range]
+      institutional_adjustment.end_range = params[:institutional_adjustment][:end_range]
+      institutional_adjustment.employer_contribution = params[:institutional_adjustment][:employer_contribution]
+      institutional_adjustment.employee_contribution = params[:institutional_adjustment][:employee_contribution]
+      institutional_adjustment.period_of_time = params[:institutional_adjustment][:period_of_time]
+      institutional_adjustment.description = params[:institutional_adjustment][:description]
+      institutional_adjustment.date_of_effectivity = params[:institutional_adjustment][:date_of_effectivity]
+      institutional_adjustment.save!
       flash[:general_flash_notification_type] = 'affirmative'
     rescue => ex
-      puts ex
       flash[:general_flash_notification] = 'Error Occurred. Please contact Administrator.'
     end
     redirect_to :action => 'index'
   end
 
   def delete
-    baseRateToBeDeleted = BaseRate.find(params[:id])
-    baseRateOwner = Employee.find(baseRateToBeDeleted.employee_id)
-    flash[:general_flash_notification] = baseRateOwner.actor.name + '\'s base rate of ' + baseRateToBeDeleted.amount.to_s + ' per ' + baseRateToBeDeleted.period_of_time + ' has been successfully deleted.'
-    flash[:general_flash_notification_type] = 'affirmative'
-    baseRateToBeDeleted.destroy
+    institutional_adjustment_to_be_deleted = InstitutionalAdjustment.find(params[:id])
+    flash[:general_flash_notification] = institutional_adjustment_to_be_deleted.id + ' has been successfully deleted'
+    institutional_adjustment_to_be_deleted.destroy
     redirect_to :action => 'index'
   end
 
   def update
-    baseRate = BaseRate.find(params[:base_rate][:id])
-    flash[:general_flash_notification] = 'Base Rate Updated'
-    process_base_rate_form(baseRate)
+    institutional_adjustment = InstitutionalAdjustment.find(params[:institutional_adjustment][:id])
+    flash[:general_flash_notification] = 'Institutional Adjustment Updated'
+    process_institutional_adjustment_form(institutional_adjustment)
   end
 
   def create
-    baseRate = BaseRate.new()
-    flash[:general_flash_notification] = 'Base Rate Created'
-    process_base_rate_form(baseRate)
+    institutional_adjustment = InstitutionalAdjustment.new()
+    flash[:general_flash_notification] = 'Institutional Adjustment Created'
+    process_institutional_adjustment_form(institutional_adjustment)
   end
 
 end
