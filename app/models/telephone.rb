e778d7a6c454c20e494ad498a6c2fc6dcf504c99
James Patrick G. Pe <jamespatrickpe@gmail.com>
2016-01-25 14:39:15 +0800

    fdadsfas

--- app/models/telephone.rb
+++ app/models/telephone.rb
@@ -1,6 +1,7 @@
 class Telephone < ActiveRecord::Base
   include UUIDHelper
-  belongs_to :actor
+  belongs_to :rel_model, polymorphic: true
+
   # validates_presence_of :actor
   validates_length_of :digits , maximum: 32, message: "digits must be less than 32 characters"
   validates_length_of :description , maximum: 256, message: "description must be less than 256 characters"
