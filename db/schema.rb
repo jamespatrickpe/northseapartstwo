717eef5a554808bc6d0e83757dd755bbe7de7fec
James Patrick G. Pe <jamespatrickpe@gmail.com>
2016-01-26 17:43:45 +0800

    fdsafsd

--- db/schema.rb
+++ db/schema.rb
@@ -131,7 +131,7 @@ ActiveRecord::Schema.define(version: 20160125053943) do
     t.string   "remark",              limit: 256
     t.boolean  "active",              limit: 1,   default: false
     t.string   "employee_id",         limit: 36
-    t.datetime "date_of_effectivity",             default: '2016-01-25 17:02:33'
+    t.datetime "date_of_effectivity",             default: '2016-01-26 10:21:05'
     t.datetime "created_at",                                                      null: false
     t.datetime "updated_at",                                                      null: false
   end
@@ -144,16 +144,12 @@ ActiveRecord::Schema.define(version: 20160125053943) do
   end
 
   create_table "expenses", force: :cascade do |t|
-    t.string   "employee_id",          limit: 36
-    t.boolean  "signed_type",          limit: 1
-    t.decimal  "amount",                           precision: 16, scale: 2
-    t.string   "period_of_time",       limit: 64
-    t.string   "rate_type",            limit: 64,                           default: "other"
-    t.string   "remark",               limit: 256
-    t.datetime "start_of_effectivity"
-    t.datetime "end_of_effectivity"
-    t.datetime "created_at",                                                                  null: false
-    t.datetime "updated_at",                                                                  null: false
+    t.decimal  "amount",                          precision: 16, scale: 2
+    t.string   "category",            limit: 256
+    t.string   "remark",              limit: 256
+    t.datetime "date_of_effectivity"
+    t.datetime "created_at",                                               null: false
+    t.datetime "updated_at",                                               null: false
   end
 
   create_table "file_sets", force: :cascade do |t|
@@ -212,8 +208,8 @@ ActiveRecord::Schema.define(version: 20160125053943) do
   create_table "leaves", force: :cascade do |t|
     t.string   "employee_id",          limit: 36
     t.string   "type_of_leave",        limit: 64
-    t.datetime "start_of_effectivity",             default: '2016-01-25 17:02:35'
-    t.datetime "end_of_effectivity",               default: '2016-01-25 17:02:35'
+    t.datetime "start_of_effectivity",             default: '2016-01-26 10:21:09'
+    t.datetime "end_of_effectivity",               default: '2016-01-26 10:21:09'
     t.string   "remark",               limit: 256
     t.datetime "created_at",                                                       null: false
     t.datetime "updated_at",                                                       null: false
@@ -242,7 +238,7 @@ ActiveRecord::Schema.define(version: 20160125053943) do
     t.string   "employee_id",         limit: 36
     t.string   "article",             limit: 255, default: "BASE"
     t.boolean  "applicability",       limit: 1,   default: false
-    t.datetime "date_of_effectivity",             default: '2016-01-25 17:02:35'
+    t.datetime "date_of_effectivity",             default: '2016-01-26 10:21:09'
     t.string   "remark",              limit: 256
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -287,7 +283,7 @@ ActiveRecord::Schema.define(version: 20160125053943) do
   create_table "regular_work_periods", force: :cascade do |t|
     t.time     "start_time",                      default: '2000-01-01 08:00:00'
     t.time     "end_time",                        default: '2000-01-01 17:00:00'
-    t.datetime "date_of_effectivity",             default: '2016-01-25 17:02:31'
+    t.datetime "date_of_effectivity",             default: '2016-01-26 10:21:03'
     t.string   "remark",              limit: 256
     t.string   "employee_id",         limit: 36
     t.datetime "created_at",                                                      null: false
