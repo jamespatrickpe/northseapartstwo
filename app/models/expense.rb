99d49b51a942806349ad106f98bcf872bb37d7e0
Jc Gofredo <john.gofredo@novare.com.hk>
2016-01-29 19:05:37 +0800

    Constant date_of_effectivity adjustment

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,10 @@ class Expense < ActiveRecord::Base
 
   include UUIDHelper
 
+  validates_numericality_of :amount
+  validates_presence_of :category
+  validates_length_of :remark , maximum: 256, message: "description must be less than 256 characters"
+  validates_presence_of :date_of_effectivity
+  validates_length_of :physical_id , maximum: 256, message: "description must be less than 256 characters"
+
 end
