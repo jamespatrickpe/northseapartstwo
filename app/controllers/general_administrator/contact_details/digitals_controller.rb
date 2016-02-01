98ba9bd7dbe1e366062c02100a0331c06990d1a8
Jc Gofredo <john.gofredo@novare.com.hk>
2016-02-01 23:01:32 +0800

    Contact Detail form mapping adj

--- app/controllers/general_administrator/contact_details/digitals_controller.rb
+++ app/controllers/general_administrator/contact_details/digitals_controller.rb
@@ -39,7 +39,8 @@ class GeneralAdministrator::ContactDetails::DigitalsController < GeneralAdminist
   def process_digital_form(myDigital)
     begin
       myDigital[:url] = params[:digital][:url]
-      myDigital[:actor_id] = params[:digital][:actor_id]
+      myDigital[:rel_model_type] = params[:digital][:rel_model_type]
+      myDigital[:rel_model_id] = params[:digital][:rel_model_id]
       myDigital[:description] = params[:digital][:description]
       myDigital.save!
       flash[:general_flash_notification_type] = 'affirmative'
