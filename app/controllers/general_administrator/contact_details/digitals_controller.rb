c5369565931f2024eb8b88c1fc6656fe002eb152
Jc Gofredo <john.gofredo@novare.com.hk>
2016-02-16 15:17:29 +0800

    Dynamic related actor/branch for Digital form

--- app/controllers/general_administrator/contact_details/digitals_controller.rb
+++ app/controllers/general_administrator/contact_details/digitals_controller.rb
@@ -28,16 +28,44 @@ class GeneralAdministrator::ContactDetails::DigitalsController < GeneralAdminist
   def new
     initialize_form
     @selected_digital = Digital.new
-    @actors = Actor.all().order('name ASC')
-    @branches = Branch.all().order('name ASC')
+    @actors1 = Actor.all()
+    @actors2 = Branch.all()
     generic_singlecolumn_form(@selected_digital)
   end
 
   def edit
     initialize_form
     @selected_digital = Digital.find(params[:id])
-    @actors = Actor.all().order('name ASC')
-    @branches = Branch.all().order('name ASC')
+    @actorsInvolved ||= []
+
+    # @digital_actor_rel = TelephonesActor.find_by_digital_id(params[:id])
+    @digital_actor_rel = DigitalsActor.where("digitals_actors.digital_id = ?", "#{params[:id]}")
+
+    involvedActorObjects ||= []
+    involvedBranchObjects ||= []
+
+    @digital_actor_rel.each do |ea|
+      if Actor.exists?(ea[:actor_id])
+        involvedActorObjects.push(Actor.find(ea[:actor_id]))
+      else
+        puts 'ID in use does not belong to an Actor'
+      end
+    end
+
+    @digital_actor_rel.each do |ea|
+      if Branch.exists?(ea[:actor_id])
+        involvedBranchObjects.push(Branch.find(ea[:actor_id]))
+      else
+        puts 'ID in use does not belong to a Branch'
+      end
+    end
+
+    @actorsInvolved = involvedActorObjects + involvedBranchObjects
+
+    @actors1 = Actor.all()
+    @actors2 = Branch.all()
+
+    @actorsInvolved.compact.uniq!
     generic_singlecolumn_form(@selected_digital)
   end
 
@@ -45,6 +73,13 @@ class GeneralAdministrator::ContactDetails::DigitalsController < GeneralAdminist
     digital_to_be_deleted = Digital.find(params[:id])
     flash[:general_flash_notification] = 'Digital information url:  ' + digital_to_be_deleted.url + ' has been deleted.'
     flash[:general_flash_notification_type] = 'affirmative'
+
+    # deletes all related actors with the digital before deleting the actual digital object
+    mapped_digital_actors = TelephonesActor.where("digitals_actors.digital_id = ?", "#{params[:id]}")
+    mapped_digital_actors.each do |a|
+      a.destroy
+    end
+
     digital_to_be_deleted.destroy
     redirect_to :action => "index"
   end
@@ -52,10 +87,18 @@ class GeneralAdministrator::ContactDetails::DigitalsController < GeneralAdminist
   def process_digital_form(myDigital)
     begin
       myDigital[:url] = params[:digital][:url]
-      myDigital[:rel_model_type] = params[:digital][:rel_model_type]
-      myDigital[:rel_model_id] = params[:digital][:rel_model_id]
       myDigital[:description] = params[:digital][:description]
       myDigital.save!
+
+      # after creating the new digital, iterate through all the actors involved and maps them with the digital
+      actorsInvolved = params[:digital][:digitalactors]
+      actorsInvolved.each_with_index do |p , index|
+        actor = DigitalsActor.new
+        actor[:actor_id] = actorsInvolved.values[index]
+        actor[:digital_id] = myDigital.id
+        actor.save!
+      end
+
       flash[:general_flash_notification_type] = 'affirmative'
     rescue => ex
       puts ex
