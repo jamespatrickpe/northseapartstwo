9d807f50154eaecba4cd958ae9ff9f6ca62e047b
joojieman <jamespatrickpe@gmail.com>
2016-02-19 08:02:40 +0800

    why

--- app/helpers/generic_render_helper.rb
+++ app/helpers/generic_render_helper.rb
@@ -1,5 +1,9 @@
 module GenericRenderHelper
 
+  def payment_scheme_date(date_aspect, selected_employee, date_of_attendance, current_attendance_date_time)
+    render(:partial => 'human_resources/compensation_and_benefits/payrolls/payment_scheme_date', :locals => {:link => link, :id => id})
+  end
+  
   def generic_payroll_add_link(link,id)
     render(:partial => 'core_partials/generic_payroll_add_link', :locals => {:link => link, :id => id})
   end
