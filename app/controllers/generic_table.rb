6a70f44e419e3ebccd2ba521c17d1b309bf78a56
James Patrick G. Pe <jamespatrickpe@gmail.com>
2016-04-11 10:33:29 +0800

    dfasdfasdf

--- app/controllers/generic_table.rb
+++ app/controllers/generic_table.rb
@@ -1,20 +1,5 @@
 module GenericTable
 
-  # Shifts the ASC/DESC on the header of table
-  def shift_table_orientation
-    table_orientation = Hash.new()
-    table_orientation["order_orientation"] = ""
-    table_orientation["orientation_symbol"] = ""
-    if( params[:order_orientation] == "ASC" )
-      table_orientation["order_orientation"] = "DESC"
-      table_orientation["orientation_symbol"] = '&#x25BC;'
-    else
-      table_orientation["order_orientation"] = "ASC"
-      table_orientation["orientation_symbol"] = '&#x25B2;'
-    end
-    return table_orientation
-  end
-
   def generic_table_aggregated_queries( mysql_table_name, mysql_created_at, my_order = 'DESC', my_limit = '10')
     order_parameter = aggregated_search_queries(params[:order_parameter], mysql_table_name, 'order_parameter' , mysql_created_at)
     order_orientation = aggregated_search_queries(params[:order_orientation], mysql_table_name, 'order_orientation', my_order)
