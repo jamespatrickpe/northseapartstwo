a7b1252df0694b1adc09fcf43cd41a1fa7ef2ab1
joojieman <jamespatrickpe@gmail.com>
2016-05-31 09:28:01 +0800

    gfgsdgf gs

--- app/controllers/general_administration/import_contacts_wizard_controller.rb
+++ app/controllers/general_administration/import_contacts_wizard_controller.rb
@@ -16,9 +16,32 @@ class GeneralAdministration::ImportContactsWizardController < GeneralAdministrat
   end
 
   def update
+    require 'roo'
 
     case step
       when :upload_excel
+
+        xlsx = Roo::Spreadsheet.open(params[wizard_path][:file])
+        primary_sheet = xlsx.sheet(0)
+        primary_sheet.each(system_actor_name: 'system_actor_name',
+                           system_actor_remark: 'system_actor_remark',
+                           telephone_digits: 'telephone_digits',
+                           telephone_remark: 'telephone_remark',
+                           digital: 'digital',
+                           digital_remark: 'digital_remark',
+                           address_remark: 'address_remark' ) do |hash,index|
+
+
+          unless index == 1
+            my_system_actor = SystemActor.new
+            my_system_actor[:name] = hash[:system_actor_name]
+            my_system_actor[:remark] = hash[:system_actor_remark]
+            my_system_actor.save!
+          end
+
+          puts hash
+
+        end
       when :show_results
     end
     render_wizard
