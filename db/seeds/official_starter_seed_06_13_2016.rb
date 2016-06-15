a9797bdb354eb19b2c253788213a999b04cbce5e
joojieman <jamespatrickpe@gmail.com>
2016-06-15 08:18:59 +0800

    dfasfds

--- db/seeds/official_starter_seed_06_13_2016.rb
+++ db/seeds/official_starter_seed_06_13_2016.rb
@@ -3,4 +3,15 @@ module OfficialStarterSeed06132016
 
   puts " == Loading Official Seed Starter Package 06/13/16 =="
 
+  mobile = ExpenseCategory.new
+  mobile.parent_expense_id = telephone.id
+  mobile.name = 'Mobile'
+  mobile.save!
+
+  landline = ExpenseCategory.new
+  landline.parent_expense_id = telephone.id
+  landline.name = 'Landline'
+  landline.save!
+
+
 end
