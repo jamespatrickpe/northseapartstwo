79a4c9f88e7a18de4f9727f6635a130ca6c59f5f
James Patrick G. Pe <jamespatrickpe@gmail.com>
2016-06-22 14:22:28 +0800

    dfasdfasdfd

--- app/controllers/accounting_and_finance/expenses_controller.rb
+++ app/controllers/accounting_and_finance/expenses_controller.rb
@@ -13,8 +13,56 @@ class AccountingAndFinance::ExpensesController < AccountingAndFinanceController
         ]
   end
 
+  def get_expense_entries(start_period, end_period, category_id)
+
+    query = Sunspot.search(ExpenseEntry) do
+      with(:datetime_of_implementation).between(start_period..end_period)
+      with(:expense_category_id).equal_to(category_id)
+    end
+    query.results
+
+  end
+
   def process_form
-    # redirect_to :action => 'index'
+    start_period = (params[:start_period])
+    end_period = (params[:end_period])
+    expense_list_interval = (params[:expense_list_interval])
+
+    case expense_list_interval
+      when "second"
+        expense_list_interval = '%Y/%m/%d %T'
+      when "minute"
+        expense_list_interval = '%Y/%m/%d %H:%M'
+      when "hour"
+        expense_list_interval = '%Y/%m/%d %H'
+      when "day"
+        expense_list_interval = '%Y/%m/%d'
+      when "week"
+        expense_list_interval = '%G/W%V'
+      when "month"
+        expense_list_interval = '%Y/%m'
+      when "year"
+        expense_list_interval = '%Y'
+      else
+        expense_list_interval = '%Y/%m/%d %T'
+    end
+
+    data_array = Array.new()
+    params[:node_id].each do |key, value|
+      unless (value == "none")
+
+
+        current_values = get_expense_entries(start_period, end_period, key)
+
+        data_array.push(current_values)
+      end
+    end
+
+    puts data_array.inspect
+    puts " ================ lala ==================="
+
+
+    #redirect_to :action => 'index', :expense_list_interval => expense_list_interval
   end
 
 end
--- app/helpers/accounting_and_finance/expenses_helper.rb
+++ app/helpers/accounting_and_finance/expenses_helper.rb
@@ -4,15 +4,4 @@ module AccountingAndFinance::ExpensesHelper
     render :template => 'accounting_and_finance/expenses/expense_list_item', :locals => {:node => node}
   end
 
-  def get_expense_entries(start_period, end_period, category_id)
-
-    query = Sunspot.search(ExpenseEntry) do
-      with(:datetime_of_implementation).between(start_period..end_period)
-      with(:expense_category_id).equal_to(category_id)
-    end
-
-    query.results
-
-  end
-
 end
