10cd081e4c63b461afb22223c6a68aaad0875632
joojieman <jamespatrickpe@gmail.com>
2016-06-28 20:24:02 +0800

    dfasfs

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,20 +4,16 @@ class Employee < ActiveRecord::Base
 
   belongs_to :system_account
   has_many :duty_status
-  has_many :restdays
+  has_many :rest_day
   has_many :attendances
   has_one :regular_work_period
 
   searchable do
 
-    text :system_account_name do
-      system_account.name
+    text :system_account_names do |employee|
+      employee.system_account.name
     end
 
-  end
-
-  def system_account_name(name)
-    SystemAccount.find_by_name(name)
   end
 
   validates_presence_of :system_account
